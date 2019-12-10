b3c55e562ee7827d415f80cf4a4d1ac17323b0aa
mortalliao <mortalliaoyujian@163.com>
2019-12-10 23:35:15 +0800

    vue router

---
+++ src/components/Hi.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hi",
+  data() {
+    return {
+      msg: "This is Hi page."
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
